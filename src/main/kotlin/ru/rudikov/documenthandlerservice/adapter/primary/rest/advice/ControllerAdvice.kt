22c1ed7b11ccfc1735aae6e9fcb14d344ed4de77
e.rudikov <aka.09@mail.ru>
2023-04-08 21:11:48 +0300

    add open-api

--- src/main/kotlin/ru/rudikov/documenthandlerservice/adapter/primary/rest/advice/ControllerAdvice.kt
+++ src/main/kotlin/ru/rudikov/documenthandlerservice/adapter/primary/rest/advice/ControllerAdvice.kt
@@ -1,21 +1,43 @@
 package ru.rudikov.documenthandlerservice.adapter.primary.rest.advice
 
-import org.springframework.http.HttpStatus
+import io.swagger.v3.oas.annotations.Hidden
+import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
+import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
+import ru.rudikov.documenthandlerservice.application.domain.exception.StorageException
 import ru.rudikov.documenthandlerservice.application.domain.exception.StorageFileNotFoundException
 
 @RestControllerAdvice
 
 class ControllerAdvice {
 
+    @Hidden
     @ExceptionHandler(value = [StorageFileNotFoundException::class])
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ResponseStatus(value = NOT_FOUND)
     fun notFoundException(ex: StorageFileNotFoundException, request: WebRequest?): ErrorMessage = ErrorMessage(
-        status = HttpStatus.NOT_FOUND.value(),
+        status = NOT_FOUND.value(),
         message = ex.message ?: "Message is not present",
-        description = HttpStatus.NOT_FOUND.reasonPhrase,
+        description = NOT_FOUND.reasonPhrase,
+    )
+
+    @Hidden
+    @ExceptionHandler(value = [StorageException::class])
+    @ResponseStatus(value = INTERNAL_SERVER_ERROR)
+    fun storageException(ex: StorageException, request: WebRequest?): ErrorMessage = ErrorMessage(
+        status = INTERNAL_SERVER_ERROR.value(),
+        message = ex.message ?: "Message is not present",
+        description = INTERNAL_SERVER_ERROR.reasonPhrase,
+    )
+
+    @Hidden
+    @ExceptionHandler(value = [Throwable::class])
+    @ResponseStatus(value = INTERNAL_SERVER_ERROR)
+    fun throwable(ex: Throwable, request: WebRequest?): ErrorMessage = ErrorMessage(
+        status = INTERNAL_SERVER_ERROR.value(),
+        message = ex.message ?: "Message is not present",
+        description = INTERNAL_SERVER_ERROR.reasonPhrase,
     )
 }
